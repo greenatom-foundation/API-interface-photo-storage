867e211a17d00cd86f198e60d66551abf3a7deb0
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2021-12-01 18:06:53 +0300

    sqlite database initial

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
 import imghdr
 import os
 from flask import Flask, render_template, request, redirect, url_for, abort, \
-    send_from_directory
+    send_from_directory, jsonify, Response
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
+
+
+def initDB():
+    db_path = os.path.join(os.path.dirname(__file__), 'app.db')
+    db_uri = 'sqlite:///{}'.format(db_path)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db = SQLAlchemy(app)
+    db.create_all()
+    if len(list(db.engine.execute("SELECT * FROM sqlite_master where type = 'table'").fetchall())) == 0:
+        db.engine.execute("CREATE TABLE T_LINK"
+                          "("
+                          " PK_ID           VARCHAR(36) NOT NULL,"
+                          " PV_PATH         VARCHAR(255) NOT NULL,"
+                          " PRIMARY KEY (PK_ID)"
+                          ")")
+
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'uploads'
+initDB()
+
 
 def validate_image(stream):
     header = stream.read(512)
@@ -17,14 +36,17 @@ def validate_image(stream):
         return None
     return '.' + (format if format != 'jpeg' else 'jpg')
 
+
 @app.errorhandler(413)
 def too_large(e):
     return "File is too large", 413
+
 
 @app.route('/')
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
     return render_template('upload.html', files=files)
+
 
 @app.route('/', methods=['POST'])
 def upload_files():
@@ -38,9 +60,11 @@ def upload_files():
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return '', 204
 
+
 @app.route('/uploads/<filename>')
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1')
