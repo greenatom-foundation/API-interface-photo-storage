4fcc06be0a00ef031eabf2b5ec0448ac0836acf5
andrros3030 <66036268+andrros3030@users.noreply.github.com>
2021-12-01 19:31:25 +0300

    uuid + t_link

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import imghdr
 import os
+import uuid
 from flask import Flask, render_template, request, redirect, url_for, abort, \
     send_from_directory, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
@@ -9,14 +10,15 @@ from werkzeug.utils import secure_filename
 def initDB():
     db_path = os.path.join(os.path.dirname(__file__), 'app.db')
     db_uri = 'sqlite:///{}'.format(db_path)
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     db = SQLAlchemy(app)
     db.create_all()
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     if len(list(db.engine.execute("SELECT * FROM sqlite_master where type = 'table'").fetchall())) == 0:
         db.engine.execute("CREATE TABLE T_LINK"
                           "("
                           " PK_ID           VARCHAR(36) NOT NULL,"
                           " PV_PATH         VARCHAR(255) NOT NULL,"
+                          " V_NAME          VARCHAR(255) NULL,"
                           " PRIMARY KEY (PK_ID)"
                           ")")
     if not os.path.exists(os.path.join(os.path.dirname(__file__), 'uploads')):
@@ -27,6 +29,7 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'uploads'
+db = SQLAlchemy(app)
 initDB()
 
 
@@ -59,7 +62,11 @@ def upload_files():
         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
                 file_ext != validate_image(uploaded_file.stream):
             return "Invalid image", 400
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        # print('filename: '+filename)
+        lnk = str(uuid.uuid4())
+        db.engine.execute("INSERT INTO T_LINK(PK_ID, PV_PATH, V_NAME) VALUES('{}', '{}', '{}')".format(lnk, os.path.join(app.config['UPLOAD_PATH'], lnk), filename))
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], lnk))
+        # print('data: ' + str(db.engine.execute('SELECT * FROM T_LINK').fetchall()))
     return '', 204
 
 
